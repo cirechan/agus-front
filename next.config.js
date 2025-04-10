d8fe7a9dadbb167c650fdaa8c52776dcac4600e7
alexPiensaenweb <alexcmadrid2001@gmail.com>
2025-04-10 20:20:52 +0200

    ajustes vercel

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost', 'agus-back.onrender.com'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
