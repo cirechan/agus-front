096119866569c753f837e94bd3870d4711327875
cirechan <59828050+cirechan@users.noreply.github.com>
2025-04-11 09:08:13 +0200

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,12 +12,12 @@ export default function DashboardLayout({
   return (
     <SidebarProvider>
       <AppSidebar />
-      <div className="flex flex-col min-h-screen md:pl-16">
+      <SidebarInset>
         <SiteHeader />
         <main className="flex-1 p-4 md:p-6">
           {children}
         </main>
-      </div>
+      </SidebarInset>
     </SidebarProvider>
   )
 }
