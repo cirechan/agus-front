9f732cf6d293427720db259858b077b3c49ab5ab
alexPiensaenweb <alexcmadrid2001@gmail.com>
2025-04-14 19:08:27 +0200

    Update page.tsx

--- src/app/dashboard/horarios/editar/[id]/page.tsx
+++ src/app/dashboard/horarios/editar/[id]/page.tsx
@@ -86,14 +86,14 @@ export default function EditarPartidoPage() {
         
         // Cargar equipos
         const equiposResponse = await equiposService.getAll()
-        setEquipos(equiposResponse.map(equipo => ({
+        setEquipos(equiposResponse.map((equipo: { _id: any; nombre: any }) => ({
           value: equipo._id,
           label: equipo.nombre
         })))
         
         // Cargar temporadas
         const temporadasResponse = await temporadasService.getAll()
-        setTemporadas(temporadasResponse.map(temporada => ({
+        setTemporadas(temporadasResponse.map((temporada: { _id: any; nombre: any }) => ({
           value: temporada._id,
           label: temporada.nombre
         })))
